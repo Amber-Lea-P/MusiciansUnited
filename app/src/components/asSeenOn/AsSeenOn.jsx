5f0bb5feca745fb3aa2ea961cfc15400ff7b8b48
Amber-Lea-P <amber.pontinen@gmail.com>
2023-03-28 12:13:55 +0300

    As seen on page

---
+++ app/src/components/asSeenOn/AsSeenOn.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./AsSeenOn.scss";
+
+const AsSeenOn = () => {
+  return (
+    <div className="asSeenOn">
+        <div className="container">
+            <span>As seen on:</span>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    </div>
+  );
+};
+
+export default AsSeenOn;
--- app/src/pages/home/Home.jsx
+++ app/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AsSeenOn from '../../components/asSeenOn/AsSeenOn'
 import Featured from '../../components/featured/Featured'
 import "./Home.scss"
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <AsSeenOn />
     </div>
   )
 }
