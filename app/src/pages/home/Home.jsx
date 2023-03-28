ea5dfc6ae66a31802c1bcd5d274efc475494a7c7
Amber-Lea-P <amber.pontinen@gmail.com>
2023-03-28 13:18:33 +0300

    completion of category card

--- app/src/pages/home/Home.jsx
+++ app/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AsSeenOn from '../../components/asSeenOn/AsSeenOn'
 import Featured from '../../components/featured/Featured'
+import Slide from '../../components/slide/Slide'
 import "./Home.scss"
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div className="home">
       <Featured />
       <AsSeenOn />
+      <Slide />
     </div>
   )
 }
