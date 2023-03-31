1c7dfbe6568fa28623bd41eb4b12fdd27f100410
Amber-Lea-P <amber.pontinen@gmail.com>
2023-03-31 13:53:18 +0300

    auth routes/ testing postman configurations

--- server/api/controllers/user.controller.js
+++ server/api/controllers/user.controller.js
@@ -1,3 +1,16 @@
- export const fn= (req, res) =>{
-   
-}
+import User from "../models/user.models.js"
+import jwt from "jsonwebtoken"
+
+ export const deleteUser = async (req, res) =>{
+
+    const user = await User.findById(req.params.id)
+   const token = req.cookies.accessToken;
+   if(!token) return res.status(401).send( "You are not authenticated.");
+
+   jwt.verify(token, process.env.JWT_KEY, (err, payload ) =>{
+    if(payload.id !== user._id){
+        return res.status(403).send( "You can delete only your account");
+    }
+})
+    //await User.findByIdAndDelete(req.params.id);
+ };
