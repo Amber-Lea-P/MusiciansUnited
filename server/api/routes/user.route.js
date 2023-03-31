dcf6671383b2d5f9b01fe6759666563758b47d14
Amber-Lea-P <amber.pontinen@gmail.com>
2023-03-31 12:37:08 +0300

    setting up register and log in

--- server/api/routes/user.route.js
+++ server/api/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-
+import { register, login, } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/test", )
-
+router.post("/register", register)
+router.post("/login", login)
 
 export default router;
