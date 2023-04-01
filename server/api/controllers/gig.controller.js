8ff9e647c9209f9ed5d2f6b147421aacbf42921f
Amber-Lea-P <amber.pontinen@gmail.com>
2023-04-01 13:12:21 +0300

    setting up search filters for gigs

--- server/api/controllers/gig.controller.js
+++ server/api/controllers/gig.controller.js
@@ -18,7 +18,7 @@ export const createGig = async (req, res, next) => {
   }
 };
 export const deleteGig = async (req, res, next) => {
-  /*try {
+    try {
     const gig = await Gig.findById(req.params.id);
     if (gig.userId !== req.userId)
       return next(createError(403, "You can delete only your gig!"));
@@ -27,19 +27,19 @@ export const deleteGig = async (req, res, next) => {
     res.status(200).send("Gig has been deleted!");
   } catch (err) {
     next(err);
-  }*/
+  }
 };
 export const getGig = async (req, res, next) => {
-  /*try {
+  try {
     const gig = await Gig.findById(req.params.id);
     if (!gig) next(createError(404, "Gig not found!"));
     res.status(200).send(gig);
   } catch (err) {
     next(err);
-  }*/
+  }
 };
 export const getGigs = async (req, res, next) => {
-  /*const q = req.query;
+  const q = req.query;
   const filters = {
     ...(q.userId && { userId: q.userId }),
     ...(q.cat && { cat: q.cat }),
@@ -56,5 +56,5 @@ export const getGigs = async (req, res, next) => {
     res.status(200).send(gigs);
   } catch (err) {
     next(err);
-  }*/
+  }
 };
