785a41fb29891beabd2056bdec3bc0d5d03117a6
Amber-Lea-P <amber.pontinen@gmail.com>
2023-04-01 07:50:38 +0300

    completion of login credentials

--- app/src/pages/login/Login.jsx
+++ app/src/pages/login/Login.jsx
@@ -1,27 +1,25 @@
 import React, {useState} from 'react'
 import "./Login.scss"
-import axios from "axios";
+import newRequest from "../../utils/newRequest.js";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const navigate = useNavigate();
   
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      const res = await axios.post("localhost:8800/api/auth/login", {
-        username,
-        password
-      });
-      console.log(res.data);
-    }catch(err) {
-      setError(err);
-      console.log(err);
+    try {
+       const res = await newRequest.post("/auth/login", { username, password });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/")
+    } catch (err) {
+      setError(err.response.data);
     }
   };
-
 
   return (
     <div className="login">
