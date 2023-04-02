1c76cefabbb02d005b94af67770d3523aee18585
Amber-Lea-P <amber.pontinen@gmail.com>
2023-04-02 15:07:57 +0300

    reviews and single review pages

--- app/src/pages/gigMusician/Gig.jsx
+++ app/src/pages/gigMusician/Gig.jsx
@@ -4,6 +4,7 @@ import { Slider } from "infinite-react-carousel/lib";
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import newRequest from '../../utils/newRequest';
+import Reviews from '../../components/reviews/Reviews';
 
 function Gig() {
 
@@ -131,7 +132,7 @@ return (
               </div>
             </div>
           )}
-
+         <Reviews gigId={id} />
         </div>
         <div className="right">
           <div className="price">
