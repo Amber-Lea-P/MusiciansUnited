711c780e2f6652c677bdf55020dfdc690f25f6e8
Amber-Lea-P <amber.pontinen@gmail.com>
2023-04-08 16:18:21 +0300

    testing different deployment platform

--- server/api/server.js
+++ server/api/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => res.send("Welcome to our API"));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173", credentials: true}));
+app.use(cors({origin:"https://musicians-united-mu.netlify.app", credentials: true}));
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
